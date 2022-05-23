8906fd872a5304983645aa86b6aef57b80550659
Kateryna Danylo <aleksandrova414@gmail.com>
2022-05-23 11:35:12 +0300

    add new file
    add new file

---
+++ main.js
@@ -0,0 +1,8 @@
+/* boolean; true/false, logical type of data;
+ number; 0, 15, -15, NaN,   
+ string; "hfds" 'def' `frfr`- template string (шаблонні строки)
+ object; контейнер з даними 
+ symbol; 
+ null;
+ underfind;
+ bigint; */ 
