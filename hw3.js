c1cc665a06bbf04fce0983f3727101061a6baff3
Kateryna Danylo <aleksandrova414@gmail.com>
2022-05-30 05:08:41 +0300

    add my homework
    add new home work

---
+++ hw3.js
@@ -0,0 +1,33 @@
+// TASK 1
+// const arrNums = [];
+// const arrResult = [];
+// const arrDoubleNumber= [11, 22, 33, 44, 55, 66, 77, 88];
+
+// arrNums[0]= +prompt( "enter the first number in the range 10 to 99");
+// arrNums[1]= +prompt( "enter the second number in the range 10 to 99 ");
+
+// do{
+// for (arrNums[1]; arrNums[1]<=arrNums[0]; arrNums[1]--){
+//      arrResult.push();
+//  } 
+//  if (arrResult.includes(arrDoubleNumber)){
+//     console.log("good");
+//  }} while (((arrNums[0] <= 10 && arrNums[0] >=99)||((arrNums[1] <= 10 && arrNums[1] >99)))==false);
+
+
+
+
+
+// TASK 2
+// let charactersNumberArr = [];
+
+// charactersNumberArr[0] = prompt("Enter characters");
+// charactersNumberArr[1] = +prompt("Enter Number");
+// if (isNaN(charactersNumberArr[1]) === true) {
+//     alert("you must enter only number");
+// } else {
+//     for (let i = 0; i < charactersNumberArr[1]; i++) {
+//         console.log(charactersNumberArr[0])
+//     };
+// }
+// TASK 3
