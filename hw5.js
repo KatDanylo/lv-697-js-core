9a9e6233c2fd2459076fbf08ffd75e1b7d2943dc
Kateryna Danylo <aleksandrova414@gmail.com>
2022-06-15 02:20:31 +0300

    change some code

--- hw5.js
+++ hw5.js
@@ -1,42 +1,56 @@
-let rooms = [['XXX', 1], ['XXXXXX', 6], ['X', 2], ['XXXXXX', 8]];
+let rooms = [
+    ['XXX', 1],
+    ['X', 2],
+  
+];
 //let countPeople = rooms.shift().length;
 let datas = [];
-let haveCountChair = rooms.slice(-1);
- for (let i=0; i<rooms.length; i++){
-     for (let j=0; j< rooms[i].length; j++ ){
-        console.log(rooms[i][j]);
-    } 
+// let haveCountChair = rooms.slice(-1);
+for (let i = 0; i < rooms.length; i++) {
+    // for (let j = 1; j < rooms[i].length; j++) {
+        // datas.push(rooms[i][j]);
+        // console.log(datas);
+        let countPeople = rooms[i][0].length ;
+        console.log(countPeople);
+        let countChair = rooms[i][1];
+        console.log(countChair);
+        let diff = countPeople-countChair;
+        console.log(diff);
+        let result = diff ;
+        console.log(result);
 }
-function meeting (allCountPeople, allCountChair) {
-   if(allCountChair < allCountPeople){
-       return "Not enough!"
-   }
-   "Game On!"
+
+
+function meeting(allCountPeople, allCountChair) {
+    if (allCountChair < allCountPeople) {
+        return "Not enough!"
+    }
+    "Game On!"
 }
 
-(function(){
-    let num1 = +prompt("enter first number");
-let num2 = +prompt("enter second number");
-let op = prompt("enter one of operators +,-,*,/ ");
-let result;
-
-switch(op){
-    case '+':
-        result = num1+num2;
-        break;
-    case '-':
-        result = num1-num2;
-        break;
-    case '*':
-        result = num1*num2;
-        break;
-    case '/':
-        result = num1/num2;
-        break;
-    default:
-        result = "you must choose oparition!";
-}
-alert(`Result: ${num1} ${op} ${num2} = ${result}`);
-})();
 
 
+// (function () {
+//     let num1 = +prompt("enter first number");
+//     let num2 = +prompt("enter second number");
+//     let op = prompt("enter one of operators +,-,*,/ ");
+//     let result;
+
+//     switch (op) {
+//         case '+':
+//             result = num1 + num2;
+//             break;
+//         case '-':
+//             result = num1 - num2;
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             break;
+//         case '/':
+//             result = num1 / num2;
+//             break;
+//         default:
+//             result = "you must choose oparition!";
+//     }
+//     alert(`Result: ${num1} ${op} ${num2} = ${result}`);
+// })();
---
+++ hw6.js
@@ -0,0 +1,41 @@
+// const btnPlus = document.getElementById('plus');
+// const btnMinus = document.getElementById('minus');
+// const inp = document.getElementById('result');
+// let currentCount = +inp.value;
+
+// function plusCount(currentCount) {
+//     return currentCount ++;
+// }
+
+// function minusCount(currentCount) {
+//     return currentCount --;
+// }
+
+
+// btnPlus.addEventListener('click', plusCount);
+// btnMinus.addEventListener('click', minusCount);
+// inp.textContent = currentCount;
+    
+
+
+
+
+// const circles = document.querySelectorAll(".circle");
+// let activeLight = 0;
+// const btnLight = document.querySelector(".btn_light");
+
+// function addActiveClass(){
+//     circles[activeLight].classList = 'circle';
+//     activeLight++;
+
+//     if (activeLight>2) {
+//          activeLight=0;
+//         }
+
+//     const currentLight = circles[activeLight];
+//     currentLight.classList.add(currentLight.getAttribute("color"))
+//     }
+
+//  btnLight.addEventListener('click',  addActiveClass);
+
+
