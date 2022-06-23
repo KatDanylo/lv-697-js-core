436e52f92833789a84d71ec1c6e554f669add65c
Kateryna Danylo <aleksandrova414@gmail.com>
2022-06-23 06:44:15 +0300

    add new task

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const inp =  document.querySelector('#marker_text');
+const out =  document.querySelector('#out_marker_text');
+let count = 0;
+class Marker {
+    constructor(colour, percentInk) {
+        colour = this.colour;
+        percentInk = this.percentInk;
+    }
+    print() {
+        console.log(`this text must be ${colour} colour`);
+    };
+
+    static countSign(percentInk) {
+        if (percentInk <= 0) {
+            console.log(`Marker is empty.Please add some ink`);
+        }
+        return percentInk - 0.5;
+    }
+}
+inp.addEventListener('input', sumSign);
+function sumSign (count){
+    (inp.value =' '? count=count: count++)
+    return count;
+}
+
+out.textContent = count;
+
+
+
+
 
